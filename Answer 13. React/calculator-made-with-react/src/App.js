dc7d06fc3f05ab196edb9c0abc9b1d8cfb4fef62
Shreyash Dawake <shreyashdawake1@gmail.com>
2023-05-23 17:46:21 +0530

    Added Calculator web app

---
+++ Answer 13. React/calculator-made-with-react/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [display, setDisplay] = useState('');
+
+  const handleClick = (value) => {
+    setDisplay(display + value);
+  };
+
+  const calculateResult = () => {
+    try {
+      const result = eval(display);
+      setDisplay(result);
+    } catch (error) {
+      setDisplay('Error');
+    }
+  };
+
+  const clearDisplay = () => {
+    setDisplay('');
+  };
+
+  return (
+    <div className="calculator">
+      <input type="text" value={display} readOnly />
+      <div className="buttons">
+        <div className="row">
+          <button onClick={() => handleClick('7')}>7</button>
+          <button onClick={() => handleClick('8')}>8</button>
+          <button onClick={() => handleClick('9')}>9</button>
+          <button onClick={() => handleClick('/')}>/</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleClick('4')}>4</button>
+          <button onClick={() => handleClick('5')}>5</button>
+          <button onClick={() => handleClick('6')}>6</button>
+          <button onClick={() => handleClick('*')}>*</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleClick('1')}>1</button>
+          <button onClick={() => handleClick('2')}>2</button>
+          <button onClick={() => handleClick('3')}>3</button>
+          <button onClick={() => handleClick('-')}>-</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleClick('0')}>0</button>
+          <button onClick={() => handleClick('.')}>.</button>
+          <button onClick={calculateResult}>=</button>
+          <button onClick={() => handleClick('+')}>+</button>
+        </div>
+        <div className="row">
+          <button onClick={clearDisplay}>Clear</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
